9dafce205bbe0cf080df342f83e097f83a1b440f
Josimar Goncalves <foxchanger@hotmail.com>
2022-04-29 04:41:50 -0300

    Fix Bug

--- JogoDaVelha-TicTacToe/Program.cs
+++ JogoDaVelha-TicTacToe/Program.cs
@@ -232,7 +232,7 @@ class Program
         else if (arr[1] != '1' && arr[2] != '2' && arr[3] != '3' && arr[4] != '4' && arr[5] != '5' && arr[6] != '6' && arr[7] != '7' && arr[8] != '8' && arr[9] != '9')
         {
             status = -1;
-            // jogando = false;
+            
         }
         
         #endregion
